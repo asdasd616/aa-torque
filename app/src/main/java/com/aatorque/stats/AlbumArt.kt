c3b7a87a497fb2264eecb1f040aa4e83e7ed2703
asdasd616 <asdasd616@gmail.com>
2024-11-06 06:45:16 +1300

    Update AlbumArt.kt

--- app/src/main/java/com/aatorque/stats/AlbumArt.kt
+++ app/src/main/java/com/aatorque/stats/AlbumArt.kt
@@ -114,5 +114,13 @@ abstract class AlbumArt : CarFragment() {
         )
     }
 
+    fun metaDataToArtist(medadata: MediaMetadata?): String? {
+        return medadata?.getString(
+            MediaMetadata.METADATA_KEY_ARTIST
+        ) ?: medadata?.getString(
+            MediaMetadata.METADATA_KEY_ARTIST
+        )
+    }
+
     abstract suspend fun onMediaChanged(medadata: MediaMetadata?)
 }
